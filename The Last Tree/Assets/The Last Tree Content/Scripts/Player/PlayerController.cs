e27772b6448b0ad514ae8fe93b6aa5143b5b4171
CLSethan <christianlenar.santiago@benilde.edu.ph>
2024-10-22 08:58:14 +0800

    Changed Firepoint, Fixed Prefabs folder

--- The Last Tree/Assets/The Last Tree Content/Scripts/Player/PlayerController.cs
+++ The Last Tree/Assets/The Last Tree Content/Scripts/Player/PlayerController.cs
@@ -9,9 +9,12 @@ public class PlayerController : MonoBehaviour
     private float moveSpeed;
     private PlayerControls playerControls;
     private Vector2 movement;
+    [SerializeField]
+    private Transform firePoint;
 
     private Rigidbody2D rb;
     private Animator anim;
+
 
     private void Awake()
     {
@@ -34,7 +37,8 @@ public class PlayerController : MonoBehaviour
     private void FixedUpdate()
     {
         Move();
-        RotateCharacter();
+        RotateFirepoint();
+        FlipCharacterSprite();
     }
 
     private void PlayerInput()
@@ -54,13 +58,26 @@ public class PlayerController : MonoBehaviour
         rb.MovePosition(moveToPosition);
     }
 
-    private void RotateCharacter()
+    private void RotateFirepoint()
     {
         // If there's movement input, update the rotation
         if (movement != Vector2.zero)
         {
             float angle = Mathf.Atan2(movement.y, movement.x) * Mathf.Rad2Deg;
-            transform.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+            firePoint.rotation = Quaternion.Euler(new Vector3(0, 0, angle));
+        }
+    }
+
+    void FlipCharacterSprite()
+    {
+        if (rb.velocity.x > movement.x)
+        {
+            transform.localScale = new Vector3(-1f, 1f, 1f);
+        }
+
+        if (rb.velocity.x < movement.x)
+        {
+            transform.localScale = Vector3.one;
         }
     }
 }
