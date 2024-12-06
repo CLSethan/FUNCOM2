81e08d33072162e204a74ccee13baf7830706a66
Shuckle-08 <patrickjames.geralde@benilde.edu.ph>
2024-12-06 10:38:38 +0800

    Incendium 1
    Incendium 1

--- The Last Tree/Assets/The Last Tree Content/Scripts/Managers/GameManager.cs
+++ The Last Tree/Assets/The Last Tree Content/Scripts/Managers/GameManager.cs
@@ -1,75 +1,72 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
+using UnityEditor.SearchService;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class GameManager : Singleton<GameManager>
 {
+	public static GameManager instance;
+
 	[SerializeField] WeaponManager _weaponManager;
 	[SerializeField] MenuManager _menuManager;
+	[SerializeField] WeaponUpgradeMenu _weaponUpgradeMenu;
+	[SerializeField] EnemySpawner _enemySpawner;
+	[SerializeField] PlayerExperience _playerExperience;
+	[SerializeField] Player _player;
+	[SerializeField] PlayerController _playerController;
+	[SerializeField] PlayerHealth _playerHealth;
+	[SerializeField] RectTransform _playerHealthBar;
 	[SerializeField] private GameObject NotificationBoard;
 	[SerializeField] private GameObject HowToPlayMenu;
+	[SerializeField] private GameObject OptionsMenu;
+	[SerializeField] private GameObject PauseMenu;
+	public Player player;
+	public Transform playerSpawnPoint;
+
+	public PlayerExperience PlayerExperience { get { return _playerExperience; } set { _playerExperience = value; } }
+
+	public Player Player { get { return _player; } set { _player = value; } }
+
+	public PlayerController PlayerController { get { return _playerController; } set { _playerController = value; } }
+
+	public PlayerHealth PlayerHealth { get { return _playerHealth; } set { _playerHealth = value; } }
+
+	public EnemySpawner EnemySpawner { get { return _enemySpawner; } set { _enemySpawner = value; } }
 
 	public WeaponManager WeaponManager { get { return _weaponManager; } set { _weaponManager = value; } }
 
 	public MenuManager MenuManager { get { return _menuManager; } set { _menuManager = value; } }
 
-	/*    public void StartGame()
-		{
-			ResetGameInstances();
-			Time.timeScale = 1;
-		}
+	public WeaponUpgradeMenu WeaponUpgradeMenu { get { return _weaponUpgradeMenu; } set { _weaponUpgradeMenu = value; } }
 
-		public void DefaultCharacterPick()
-		{
-			Player.characterType = CharacterType.DEFAULT;
+    private void Awake()
+    {
+		instance = this;
+    }
 
-			ResetGameInstances();
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape) && MenuManager.GetCurrentMenuType() == MenuType.InGameMenu)
+        {
+			if (!WeaponManager.isUpgradeMenuActive)
+			{
+				if (!PauseMenu.activeSelf)
+				{
+					PauseMenu.SetActive(true);
+					Time.timeScale = 0;
+				}
+				else
+				{
+					Time.timeScale = 1;
+					PauseMenu.SetActive(false);
+				}
+			}
+        }
+    }
 
-			Time.timeScale = 1;
-		}
-
-		public void SpeedCharacterPick()
-		{
-			Player.characterType = CharacterType.SPEED;
-
-			ResetGameInstances();
-
-			Time.timeScale = 1;
-		}
-
-		public void TankCharacterPick()
-		{
-			Player.characterType = CharacterType.TANK;
-
-			ResetGameInstances();
-
-			Time.timeScale = 1;
-		}
-
-		public void RestartGame()
-		{
-			ResetGameInstances();
-			Time.timeScale = 1;
-		}
-
-		public void ResetGameInstances()
-		{
-			SpawnerController.Instance.Reset();
-			Player.Reset();
-			ScoreManager.ResetScore();
-		}
-
-
-
-		public void GameOver()
-		{
-			Time.timeScale = 0;
-			MenuMgr.Instance.SwitchMenu((int)MenuType.GameOverMenu);
-			ScoreManager.Instance.UpdateGameOverScoreText();
-		}*/
-
-	public void DestroyNotificationBoard()
+    public void DestroyNotificationBoard()
 	{
 		MenuManager.buttonClickSound.Play();
 
@@ -90,10 +87,89 @@ public class GameManager : Singleton<GameManager>
 		HowToPlayMenu.SetActive(false);
 	}
 
+	public void ActivateOptionsMenu()
+	{
+		MenuManager.buttonClickSound.Play();
+
+		OptionsMenu.SetActive(true);
+	}
+
+	public void DeactivateOptionsMenu()
+	{
+		MenuManager.buttonClickSound.Play();
+
+		OptionsMenu.SetActive(false);
+	}
+
+	public void ContinueGame()
+    {
+		Time.timeScale = 1;
+		MenuManager.buttonClickSound.Play();
+		PauseMenu.SetActive(false);
+	}
+
 	public void QuitGame()
 	{
 		MenuManager.buttonClickSound.Play();
 
 		Application.Quit();
 	}
+
+	public void ResetGameInstances()
+	{
+		//Time.timeScale = 1;
+		// SceneManager.LoadScene("FinalGameScene 1");
+		Time.timeScale = 1;
+		player.transform.position = playerSpawnPoint.position;
+		EnemySpawner.ResetEnemySpawner();
+		TreeHealth.instance.ResetTreeHealth();
+		Tree.instance.ResetTreeStats();
+		PlayerExperience.ResetPlayerExperience();
+		WeaponManager.ResetWeaponManager();
+		CoinController.instance.ResetCoinController();
+		PlayerStatController.instance.ResetPlayerStatController();
+		PlayerHealth.instance.ResetPlayerHealth();
+		Player.ResetPlayerPickupRange();
+		PlayerController.ResetPlayerSpeed();
+		PauseMenu.SetActive(false);
+	}
+
+	public void PlayerDied()
+	{
+		Rigidbody2D _playerMass = player.GetComponent<Rigidbody2D>();
+		SpriteRenderer _playerSprite = player.GetComponent<SpriteRenderer>();
+		CapsuleCollider2D _playerCollider = player.GetComponent<CapsuleCollider2D>();
+
+		SFXManager.instance.PlaySFXPitched(2);
+        _playerHealthBar.localScale = new Vector2(0, 0);
+        _playerMass.mass = 1000;
+		_playerSprite.color = Color.clear;
+		_playerController.enabled = false;
+		_playerCollider.enabled = false;
+		_weaponManager.gameObject.SetActive(false);
+		EnemySpawner.playerDead();	
+        StartCoroutine(respawn());
+	}
+
+	public IEnumerator respawn()
+    {
+		Rigidbody2D _playerMass = player.GetComponent<Rigidbody2D>();
+		SpriteRenderer _playerSprite = player.GetComponent<SpriteRenderer>();
+		CapsuleCollider2D _playerCollider = player.GetComponent<CapsuleCollider2D>();
+		
+        yield return new WaitForSeconds(3f);
+
+        player.transform.localPosition = new UnityEngine.Vector2(0, 0);
+        _playerSprite.color = new Color(255f, 255f, 255f, 255f);
+        _playerHealthBar.localScale = new Vector2(1, 1);
+        _weaponManager.gameObject.SetActive(true);	
+        _playerMass.mass = 1;
+        _playerHealth.currentHealth = _playerHealth.maxHealth;
+		_playerHealth.healthSlider.value = _playerHealth.currentHealth;
+        _playerController.enabled = true;
+		_playerCollider.enabled = true;
+        EnemySpawner.playerAlive();
+    }
+
+
 }
